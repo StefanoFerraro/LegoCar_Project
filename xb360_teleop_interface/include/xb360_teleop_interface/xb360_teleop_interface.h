014828777ae136580fcb39ffe671a316d43785d9
Ubuntu <ubuntu@ubuntu.lan>
2020-10-17 15:07:14 +0000

    Readme xb360 update

--- xb360_teleop_interface/include/xb360_teleop_interface/xb360_teleop_interface.h
+++ xb360_teleop_interface/include/xb360_teleop_interface/xb360_teleop_interface.h
@@ -1,25 +1,11 @@
 /**
-Software License Agreement (BSD)
 
-\authors   Mike Purvis <mpurvis@clearpathrobotics.com>
-\copyright Copyright (c) 2014, Clearpath Robotics, Inc., All rights reserved.
+\authors   		Ferraro Stefano <ferrarostefano@rocketmail.com>
 
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that
-the following conditions are met:
- * Redistributions of source code must retain the above copyright notice, this list of conditions and the
-   following disclaimer.
- * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
-   following disclaimer in the documentation and/or other materials provided with the distribution.
- * Neither the name of Clearpath Robotics nor the names of its contributors may be used to endorse or promote
-   products derived from this software without specific prior written permission.
+\date			18/10/2020
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WAR-
-RANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, IN-
-DIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
-OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+\description	Header file
+
 */
 
 //#ifndef XB360_TELEOP_INTERFACE_XB360_TELEOP_INTERFACE_H
@@ -37,9 +23,6 @@ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSI
 namespace xb360_teleop_interface
 {
 
-/**
- * Class implementing a basic Joy -> Twist translation.
- */
 class Interface
 {
 public:
@@ -54,18 +37,23 @@ private:
 	ros::Subscriber joy_sub;
 	ros::Publisher cmd_vel_pub;
 
+	// Mapping of the XBOX360 buttons ans axes to integer values
 	std::map <int, std::string> xb360_buttons_map = {{0, "A"}, {1, "B"}, {2, "X"}, {3, "Y"}, {4, "LB"}, {5, "RB"}};
 	std::map <int, std::string> xb360_axes_map = {{0, "Left stick orizzontal direction"}, {1, "Left stick vertical direction"}, {2, "LT"}, {3, "Right stick orizzontal direction"}, {4, "Right stick vertical direction"}, {5, "RT"}};
 
+	// Function linking to buttons/axes 
   	int enable_button = 0;
 	int forward_axis = 5;
 	int backward_axis = 2;
 	int angular_axis = 0;
 
-	double out_scale = 0.5;
+	// Output scale for throttling/steering commands
+	double linear_out_scale = 0.5;
+	double angular_out_scale = 0.5;
 
 	void joyCallback(const sensor_msgs::Joy& msg);
 	double linear_motion(const double forward, const double backward);
+	double angular_motion(const double angle)
 
 
 };
--- xb360_teleop_interface/src/xb360_teleop_interface.cpp
+++ xb360_teleop_interface/src/xb360_teleop_interface.cpp
@@ -1,3 +1,15 @@
+/**
+
+\authors   		Ferraro Stefano <ferrarostefano@rocketmail.com>
+
+\date			18/10/2020
+
+\description	The package subscribe to the message `/joy` (type sensor_msgs/Joy). 
+				Sensed buttons/axes are converted into linear and angular motion.
+				A message `/cmd_vel` (type geometry_msgs/Twist) is published.
+
+*/
+
 #include <xb360_teleop_interface/xb360_teleop_interface.h>
 
 namespace xb360_teleop_interface
@@ -8,12 +20,13 @@ namespace xb360_teleop_interface
 		cmd_vel_pub = nh->advertise<geometry_msgs::Twist>("cmd_vel", 1);
 		joy_sub = nh->subscribe("/joy", 1, &Interface::joyCallback, this);
 
+		// Output bottons/axes to be used for the control
 		ROS_INFO_STREAM("Teleop enable button: " << xb360_buttons_map[enable_button]);
 		ROS_INFO_STREAM("Teleop forward axis: " << xb360_axes_map[forward_axis]);
 		ROS_INFO_STREAM("Teleop backwards axis: " << xb360_axes_map[backward_axis]);
 		ROS_INFO_STREAM("Teleop angular axis: " << xb360_axes_map[angular_axis]);
-		ROS_INFO_STREAM("linear axis (x) scale : " << out_scale);
-		ROS_INFO_STREAM("angular axis (z) scale: " << out_scale);
+		ROS_INFO_STREAM("linear axis (x) scale : " << linear_out_scale);
+		ROS_INFO_STREAM("angular axis (z) scale: " << angular_out_scale);
 	}
 
 	Interface::~Interface()
@@ -23,31 +36,40 @@ namespace xb360_teleop_interface
 
 	double Interface::linear_motion(const double forward, const double backward)
 	{
-		double frw_inscale = (-(forward - 1)/2 * out_scale);
-		double bkw_inscale = ((backward - 1)/2 * out_scale);
+		// conversion of the sensed axis into the output scale desired
+		double frw_inscale = (-(forward - 1)/2 * linear_out_scale);
+		double bkw_inscale = ((backward - 1)/2 * linear_out_scale);
 
+		// distinguish between forward and backward motion, comparing the axes values
 		if(frw_inscale > 0.1 && frw_inscale > -bkw_inscale)
 		{
-			return -frw_inscale;	//Opposite direction of the motor
+			return -frw_inscale;	// opposite direction of the motor
 		}
 
 		if(-bkw_inscale > 0.1 && frw_inscale < -bkw_inscale)
 		{
-			return -bkw_inscale;	//Opposite direction of the motor
+			return -bkw_inscale;	// opposite direction of the motor
 		}
 		else {return 0.0;}
 	}
 
+	double Interface::angular_motion(const double angle)
+	{
+		return  -angle * angular_out_scale; // opposite direction of the servo
+	}
+
 	void Interface::joyCallback(const sensor_msgs::Joy& msg)
 	{
-		if(msg.buttons[enable_button])
+		if(msg.buttons[enable_button]) // check if the enable button is active
 		{
+			// linear motion conversion
 			cmd_vel_msg.linear.x = Interface::linear_motion(msg.axes[forward_axis], msg.axes[backward_axis]);
-			cmd_vel_msg.angular.z =  - msg.axes[angular_axis] * out_scale;
+			// angular motion conversion
+			cmd_vel_msg.angular.z =  Interface::angular_motion(msg.axes[angular_axis]);
 
 			cmd_vel_pub.publish(cmd_vel_msg);
 		}
-		else
+		else	// otherwise publish a null Twist message
 		{
 			geometry_msgs::Twist cmd_vel_zero;
 			cmd_vel_pub.publish(cmd_vel_zero);			
